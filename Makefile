a7b954abec500f74f8a900f1388e3f9fb1a3ab05
Alberto Oliveira <alberto.a.oliv@gmail.com>
2018-09-14 17:27:04 -0300

    [update] new scikit-learn prototype nearest neighbors search

--- Makefile
+++ Makefile
@@ -6,31 +6,42 @@ OUTDIR?="./output/"
 QDIR?="./queries/"
 
 index: create_index.py
-		python create_index.py $(CFGPATH)
+	python create_index.py $(CFGPATH)
 
 search: search_index.py
-		python search_index.py $(CFGPATH)
+	python search_index.py $(CFGPATH)
+
+index_and_search: create_and_search_index.py
+ifndef JOBS
+	python run_experiments.py $(DATASET) $(DESCRIPTOR) $(CFGPATH)
+else
+	python run_experiments.py $(DATASET) $(DESCRIPTOR) $(CFGPATH) -s $(JOBS)
+endif
 
 rank: create_ranks.py
-		python create_ranks.py $(CFGPATH)
+	python create_ranks.py $(CFGPATH)
 
 evaluate: evaluate_and_label.py
-		python evaluate_and_label.py $(CFGPATH)
+	python evaluate_and_label.py $(CFGPATH)
 
 pipeline: index search rank evaluate
-		@echo "-- Pipeline for: "
-		@cat $(CFGFILE)
+	@echo "-- Pipeline for: "
+	@cat $(CFGFILE)
+
+pipeline_sk: index_and_search rank evaluate
+	@echo "-- Pipeline for: "
+	@cat $(CFGFILE)
 
 fitting:
-		python fitting.py $(CFGPATH)
+	python fitting.py $(CFGPATH)
 
 .PHONY: cleanout view list
 
 cleanout:
-		rm -rfv $(OUTDIR)`sed -n -e 's/expname=//p' $(CFGPATH)`
+	rm -rfv $(OUTDIR)`sed -n -e 's/expname=//p' $(CFGPATH)`
 
 view:
-		@cat $(CFGPATH)
+	@cat $(CFGPATH)
 
 list:
-		@ls $(CFGDIR)
+	@ls $(CFGDIR)
---
+++ libretrieval/create_and_search_index.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import argparse
+import glob
+
+import numpy as np
+
+from sklearn.preprocessing import normalize
+from sklearn.neighbors import NearestNeighbors
+
+from libretrieval.utility import safe_create_dir, cfgloader
+from libretrieval.features.io import load_features
+
+
+def create_and_search_index(retcfg, jobs):
+
+    q_features = load_features(retcfg['path']['qfeature'])
+    q_namelist = np.loadtxt(retcfg['path']['qlist'], dtype=dict(names=('qname', 'nfeat'), formats=('U100', np.int32)))
+
+    assert q_features.shape[0] == np.sum(q_namelist['nfeat']), "Inconsistent number of features sum and size of" \
+                                                               "query features array"
+
+    norm = retcfg.get('feature', 'norm', fallback=None)
+
+    db_features = load_features(retcfg['path']['dbfeature'])
+    if norm:
+        db_features = normalize(db_features, norm)
+        q_features = normalize(q_features, norm)
+
+    outdir = retcfg['path']['outdir'] + "queryfiles/"
+    safe_create_dir(outdir)
+
+    index_type = retcfg['index']['index_type']
+    dist_type = retcfg['index']['dist_type']
+
+    knn = retcfg.getint('search', 'knn')
+
+    print(" -- Creating <{0:s}> NN index".format(index_type))
+    print("     -> KNN: {0:d}".format(knn))
+    print("     -> Metric: {0:d}".format(dist_type))
+    nnidx = NearestNeighbors(n_neighbors=knn, algorithm=index_type, metric=dist_type, n_jobs=jobs)
+    nnidx.fit(db_features)
+    print("")
+
+    print(" -- Searching index with {0:02d} jobs".format(jobs))
+    distances, indices = nnidx.kneighbors(q_features)
+
+    s = 0
+    for qname, n in q_namelist:
+
+        qdists = distances[s:s+n]
+        qidx = distances[s:s+n]
+
+        matchfpath = "{0:s}{1:s}.matches".format(outdir, qname)
+        distfpath = "{0:s}{1:s}.dist".format(outdir, qname)
+
+        print(qname, "-> ", s, ":", s+n)
+        print("   |_ dists: ", qdists.shape)
+        print("   |_ dists: ", qidx.shape, end="\n---\n")
+        np.save(matchfpath + ".npy", qidx)
+        np.save(distfpath + ".npy", qdists)
+        s += n
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cfgfile", help="Retrieval configuration file", type=str)
+    parser.add_argument("-j", "--njobs", help="Number of concurrent jobs for searching", type=int, default=1)
+    args = parser.parse_args()
+
+    retcfg = cfgloader(args.cfgfile)
+
+    create_and_search_index(retcfg, args.njobs)
+
+
